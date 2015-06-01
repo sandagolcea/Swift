6d3e26a2ec705a73a93a33e8854e1338b62eaad1
Sanda Golcea <sanda.golcea@gmail.com>
2015-06-01 16:17:14 +0100

    Adding a SimpleApp for playing

---
+++ SimpleApp/SimpleApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SimpleApp
+//
+//  Created by Sanda Golcea on 6/1/15.
+//  Copyright (c) 2015 Sanda Golcea. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
