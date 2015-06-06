fbe86204b0739901b4f06dd50a10ed35fe13fa20
Sanda Golcea <sanda.golcea@gmail.com>
2015-06-06 23:41:57 +0100

    FlagGuess: adds logic to flags for correct answer&keeping score

--- FlagGuess/FlagGuess/ViewController.swift
+++ FlagGuess/FlagGuess/ViewController.swift
@@ -16,10 +16,10 @@ class ViewController: UIViewController {
 
     var countries = [String]()
     var score = 0
-    
+    var correctAnswer = 0
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
         countries += ["estonia", "france", "germany", "ireland", "italy", "monaco", "nigeria", "poland", "russia", "spain", "uk", "us"]
         flag1.layer.borderWidth = 1
         flag2.layer.borderWidth = 1
@@ -30,18 +30,32 @@ class ViewController: UIViewController {
         askQuestion()
     }
 
-    func askQuestion() {
+    func askQuestion(action: UIAlertAction! = nil) {
         countries.shuffle()
+        correctAnswer = Int(arc4random_uniform(3))
+        title = countries[correctAnswer].uppercaseString
         flag1.setImage(UIImage(named: countries[0]), forState: .Normal)
         flag2.setImage(UIImage(named: countries[1]), forState: .Normal)
         flag3.setImage(UIImage(named: countries[2]), forState: .Normal)
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func buttonPressed(sender: UIButton) {
+      var title : String
+        if sender.tag == correctAnswer {
+            title = "Correct"
+            score++
+        } else {
+            title = "Wrong"
+            score--
+        }
+        let alert = UIAlertController(title: title, message: "Your score is \(score).", preferredStyle: .Alert)
+        alert.addAction(UIAlertAction(title: "Continue", style: .Default, handler: askQuestion))
+        presentViewController(alert, animated: true, completion: nil)
     }
 
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
 
 }
 
