05a3a97ccc9c78a78aa956509908028af7300eb5
Sanda Golcea <sanda.golcea@gmail.com>
2015-06-06 20:35:07 +0100

    FlagGuess adds 3 random flags

--- FlagGuess/FlagGuess/ViewController.swift
+++ FlagGuess/FlagGuess/ViewController.swift
@@ -13,10 +13,28 @@ class ViewController: UIViewController {
     @IBOutlet weak var flag1: UIButton!
     @IBOutlet weak var flag2: UIButton!
     @IBOutlet weak var flag3: UIButton!
+
+    var countries = [String]()
+    var score = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        countries += ["estonia", "france", "germany", "ireland", "italy", "monaco", "nigeria", "poland", "russia", "spain", "uk", "us"]
+        flag1.layer.borderWidth = 1
+        flag2.layer.borderWidth = 1
+        flag3.layer.borderWidth = 1
+        flag1.layer.borderColor = UIColor.lightGrayColor().CGColor
+        flag2.layer.borderColor = UIColor.lightGrayColor().CGColor
+        flag3.layer.borderColor = UIColor.lightGrayColor().CGColor
+        askQuestion()
+    }
+
+    func askQuestion() {
+        countries.shuffle()
+        flag1.setImage(UIImage(named: countries[0]), forState: .Normal)
+        flag2.setImage(UIImage(named: countries[1]), forState: .Normal)
+        flag3.setImage(UIImage(named: countries[2]), forState: .Normal)
     }
 
     override func didReceiveMemoryWarning() {
