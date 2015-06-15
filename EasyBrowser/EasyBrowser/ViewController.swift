635d79c702131dd22efc91527b3c44dcc39c97cc
Sanda Golcea <sanda.golcea@gmail.com>
2015-06-15 14:03:40 +0100

    EasyBrowser: adds ActionSheet for selecting page

--- EasyBrowser/EasyBrowser/ViewController.swift
+++ EasyBrowser/EasyBrowser/ViewController.swift
@@ -14,24 +14,37 @@ class ViewController: UIViewController, WKNavigationDelegate {
     var webView: WKWebView!
 
     override func loadView() {
-        // set this view to be the navigation view; entire view is this
         webView = WKWebView()
         webView.navigationDelegate = self
         self.view = webView
     }
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
         let url = NSURL(string: "http://www.google.com")!
         webView.loadRequest(NSURLRequest(URL: url))
         webView.allowsBackForwardNavigationGestures = true
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Open", style: .Plain, target: self, action: "openTapped")
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    func openTapped() {
+        let alert = UIAlertController(title: "open page..", message: nil, preferredStyle: .ActionSheet)
+        alert.addAction(UIAlertAction(title: "google.com", style: .Default, handler: openPage))
+        alert.addAction(UIAlertAction(title: "apple.com", style: .Default, handler: openPage))
+        alert.addAction(UIAlertAction(title: "cancel", style: .Cancel, handler: nil))
+        presentViewController(alert, animated: true, completion: nil)
+    }
+    
+    func openPage(action: UIAlertAction!) {
+        let url = NSURL(string: "http://" + action.title)!
+        webView.loadRequest(NSURLRequest(URL: url))
+    }
 
 }
 
